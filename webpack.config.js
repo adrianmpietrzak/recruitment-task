7810608bfa209eed4cb7a390f5c6cb75424f4b94
Adrian Pietrzak <adrianmpietrzak@gmail.com>
2019-11-13 20:45:09 +0100

    Tidy up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,26 @@
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: ["babel-polyfill", "./src/index.js"],
 
-  // output: {
-  //   filename: 'bundle.js'
-  // },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
         test: /\.(html)$/,
         use: {
-          loader: 'html-loader',
+          loader: "html-loader",
           options: {
             minimize: true
           }
@@ -33,11 +30,11 @@ module.exports = {
         test: /\.s?[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader'
+          "sass-loader"
         ]
       }
     ]
@@ -45,22 +42,22 @@ module.exports = {
 
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
+      template: "./src/index.html",
+      filename: "./index.html"
     }),
 
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // all options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
 
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-        API_URL: JSON.stringify('https://api.nasa.gov/mars-photos/api/v1'),
-        API_KEY: JSON.stringify('5zbOSX8Xn2l632bf0Zf9UEyyEeX5dyRbK773t90j')
+      "process.env": {
+        NODE_ENV: JSON.stringify("development"),
+        API_URL: JSON.stringify("https://api.nasa.gov/mars-photos/api/v1"),
+        API_KEY: JSON.stringify("5zbOSX8Xn2l632bf0Zf9UEyyEeX5dyRbK773t90j")
       }
     })
   ]
